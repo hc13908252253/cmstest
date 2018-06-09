9fc8388df462d790809e952055ad1919fa300728
司海清 <hc13908252253@163.com>
2018-06-09 22:22:54 +0800

    默认的

---
+++ test00.php
@@ -0,0 +1,10 @@
+<?php
+header("content-type:text/html;charset=utf-8");
+/**
+ * Created by PhpStorm.
+ * User: haiqing si
+ * Date: 2018/6/9
+ * Time: 22:19
+ * 这是新 建文件，测试上传功能
+ */
+echo "测试上传功能2018/6/9  22:20";
