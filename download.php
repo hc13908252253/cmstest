bfec083e12179928c6211f052fa0d418d594aa55
司海清 <hc13908252253@163.com>
2018-06-10 21:54:18 +0800

    默认的

--- download.php
+++ download.php
@@ -6,8 +6,7 @@ if (! file_exists ( $file_dir . $file_name )) {
 	header('HTTP/1.1 404 NOT FOUND');  
 } else {    
     $file = fopen ( $file_dir . $file_name, "rb" ); 
-
-    //告诉浏览器这是一个文件流格式的文件    
+    //告诉浏览器这是一个文件流格式的文件
     Header ( "Content-type: application/octet-stream" ); 
     //请求范围的度量单位  
     Header ( "Accept-Ranges: bytes" );  
@@ -15,8 +14,7 @@ if (! file_exists ( $file_dir . $file_name )) {
     Header ( "Accept-Length: " . filesize ( $file_dir . $file_name ) );  
     //用来告诉浏览器，文件是可以当做附件被下载，下载后的文件名称为$file_name该变量的值。
     Header ( "Content-Disposition: attachment; filename=" . $file_name );    
-
-    //读取文件内容并直接输出到浏览器    
+    //读取文件内容并直接输出到浏览器
     echo fread ( $file, filesize ( $file_dir . $file_name ) );    
     fclose ( $file );    
     exit ();    
--- download_html.php
+++ download_html.php
@@ -1,12 +1,11 @@
-<center>
-<h1>文件列表</h1>
-<table border="1">
+<h1>文件列表</h1><h3>(点击文件名下载)</h3>
+<table border="1" width="80%">
 	<tr>
 		<td>序号</td>
 		<td>文件名</td>
 	</tr>
 	<?php
-	$file_dir = "./download/";
+	$file_dir = "./uploads/";  //下载目录，是本程序的下级文件夹
 	$filedatas = scandir($file_dir);
 	for($i=2;$i<count($filedatas);++$i){
 		echo "<tr>";
@@ -18,4 +17,3 @@
 	}
 	?>
 </table>
-</center>
